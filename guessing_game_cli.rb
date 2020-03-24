e5340a2020504699aca63d1ebb0efe314a04d4c0
Kathryn Clark <kathrynlael@gmail.com>
2020-03-24 02:19:02 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  random_number = rand(1...6)
+  random_number = rand(1..6)
   user_input = gets.chomp.to_s
   if user_input == random_number
     puts "You guessed the correct number!"
